da0d24b509239d0deca42741f58a87a81c41799b
Bernard Mayer <25210782+BernardMayer@users.noreply.github.com>
2018-07-11 16:10:11 +0200

    update nmeaTalkers.py
    Traiter les phrases proprietaires

--- nmeaDb/nmeaTalkers.py
+++ nmeaDb/nmeaTalkers.py
@@ -8,6 +8,7 @@ from __future__ import unicode_literals
 Extraire le talker d'une phrase NMEA 0183
 $IIVLW,0088.0,N,088.65,N*4E
 $IIVHW,,,268.,M,01.44,N,02.66,K*10
+!AIVDO,1,1,,,B3HvFrP00?uA@GVjqa@03wc5wP06,0*2D
 """
 
 import cgitb
@@ -44,13 +45,13 @@ me = sys.argv[0]
 if (len(sys.argv) == 1) :
     print("Le fichier NMEA dit etre le 1er parametre de " + me)
     quit()
-if (len(sys.argv) == 2) :
+if (len(sys.argv) >= 2) :
     nmeaFilename = sys.argv[1]
     print("Traitement du fichier NMEA " + nmeaFilename)
 
 ## Lecture du fichier
 dTalkers = dict()
-#lTalkers = list()
+dPropSentences = dict()
 if (not os.path.exists(nmeaFilename)) :
     print("Fichier", nmeaFilename, "introuvable")
     quit()
@@ -59,13 +60,26 @@ with open(nmeaFilename, 'r') as fNmea :
     for line in fNmea.readlines() :
         nLine += 1
         if (line[0:1] == "$") :
-            idTalker = line[1:3]
-            try :
-                dTalkers[idTalker] = dTalkers[idTalker] + 1
-            except :
-                dTalkers[idTalker] = 1
-            #lTalkers.append(idTalker)
-#print(dTalkers)
+            if (line[0:2] == "$P") :
+                pass
+                pos = line.find(',')
+                if (pos > 0) :
+                    propSentence = line[0:pos]
+                else :
+                    propSentence = line[0:7]
+                try :
+                    dPropSentences[propSentence] = dPropSentences[propSentence] + 1
+                except :
+                    dPropSentences[propSentence] = 1
+            else :
+                idTalker = line[1:3]
+                try :
+                    dTalkers[idTalker] = dTalkers[idTalker] + 1
+                except :
+                    dTalkers[idTalker] = 1
+                #lTalkers.append(idTalker)
+print(dTalkers)
+print(dPropSentences)
 if (len(dTalkers) > 0) :
     print("idTalker" + TAB + "Nombre")
     for (k, v) in dTalkers.items()  :
